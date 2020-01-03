d5585100acfc801076f07cd37b167398020e0148
Gary Greene <greeneg@tolharadys.net>
2020-01-02 23:37:47 -0500

    Changes:
    - tests now deal with more of the subroutines
    - BlprntParser now parses out of the working copy the following
      entities:
      - srcpkg_name
      - version
      - release
      - distribution
      - license
      - buildrequires
    - Validation of license
    - Buildrequires presented as a hash with type
      - path, validates a fully-qualified path to a file
      - unpinned_pkg, only contains a package name
      - pinned_pkg, struct contains the following:
        ```json
        {
    	"type": "pinned_pkg",
    	"cmp_expression": "<CMP_EXP>",
    	"version": "<VERSION_STRING>"
        }
        ```
    
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- lib/libLSI/BlprntParser.pm
+++ lib/libLSI/BlprntParser.pm
@@ -11,13 +11,18 @@ use feature qw(:5.30);
 # (likely won't change much though in the future...)
 no warnings "experimental::lexical_subs";
 no warnings "experimental::signatures";
+no warnings "experimental::smartmatch";
 use feature 'lexical_subs';
 use feature 'signatures';
 
 use boolean;
+use Data::Dumper;
+use List::MoreUtils;
 use Path::Tiny;
+
 use libLSI::Constants;
 use libLSI::Configuration;
+use libLSI::Errors;
 use libLSI::LicenseValidation;
 
 # my $dsl = Marpa::R2::Scanless->G->new(
@@ -246,6 +251,8 @@ Gary L. Greene, Jr. C<< greeneg at altimatos.com >>
 
 =cut
 
+our $VERSION = $libLSI::Constants::VERSION;
+
 my %keyword = (
     'blueprint' => 'Blueprint',
     'end'       => 'End'
@@ -253,8 +260,8 @@ my %keyword = (
 my $cfg = libLSI::Configuration->new();
 my $cfg_struct = $cfg->get();
 if (! defined $cfg_struct) {
-    say STDERR "ERROR: cannot read configuration file: Exiting";
-    exit 1;
+    say STDERR "ERROR: " . $_errors{'ENOENT'}->{'msg'} . ": Exiting";
+    exit $_errors{'ENOENT'}->{'code'};
 }
 
 =head1 SUBROUTINES/METHODS
@@ -297,6 +304,153 @@ sub new ($class) {
     return $self;
 }
 
+=head2 SUBROUTINE: validate_msg_level
+
+=head3 DESCRIPTION:
+
+    Subroutine to validate that the value passed in is one of the known
+    message levels, 
+
+=over
+
+=item INFO
+
+=item DEBUG
+
+=item TRACE
+
+=item NOTICE
+
+=item WARNING
+
+=item ERROR
+
+=item CRITICAL
+
+=item ALERT
+
+=item EMERGENCY
+
+=back
+
+    This is NOT an OO method, and does not take an object reference.
+
+=head3 SCOPE:
+
+=over
+
+=item Public
+
+=back
+
+=head3 ARGUMENT(S):
+
+=over
+
+=item SCALAR: the message level to validate
+
+=back
+
+=head3 RETURN VALUE(S):
+
+=over
+
+=item SCALAR: boolean
+
+    If the value passed in is a valid message level, it returns a scalar true
+    value. Otherwise, false.
+
+=cut
+sub validate_msg_level ($level) {
+    given (uc $level) {
+        when ('INFO') { return true; }
+        when ('DEBUG') { return true; }
+        when ('TRACE') { return true; }
+        when ('NOTICE') { return true; }
+        when ('WARNING') { return true; }
+        when ('ERROR') { return true; }
+        when ('CRITICAL') { return true; }
+        when ('ALERT') { return true; }
+        when ('EMERGENCY') { return true; }
+    };
+    return false;
+}
+
+=head2 SUBROUTINE: dbgmsg
+
+=head3 DESCRIPTION:
+
+    Subroutine to print out debug information based off whether the 'LSI_DEBUG'
+    environment variable is defined.
+
+    This is NOT an OO method, and does not take an object reference.
+
+=head3 SCOPE:
+
+=over
+
+=item Public
+
+=back
+
+=head3 ARGUMENT(S):
+
+=over
+
+=item SCALAR: The message string to output
+
+=item SCALAR: The message class
+
+   The level of the message to be emitted. These are based off the traditional
+   syslog levels, with the addition of the TRACE level used for tracing code
+   paths. These levels are:
+   
+=over
+
+=item INFO
+
+=item DEBUG
+
+=item TRACE
+
+=item NOTICE
+
+=item WARNING
+
+=item ERROR
+
+=item CRITICAL
+
+=item ALERT
+
+=item EMERGENCY
+
+=back
+
+=back
+
+=head3 RETURN VALUE(S):
+
+=over
+
+=item VOID
+
+=back
+
+=cut
+sub dbgmsg ($level, $msg) {
+    if (validate_msg_level($level)) {
+        $level = uc $level;
+    } else {
+        dbgmsg("ERROR", $_errors{'EINVAL'}->{'msg'} . ": Exiting");
+        exit $_errors{'EINVAL'}->{'code'};
+    }
+
+    if (defined $ENV{'LSI_DEBUG'}) {
+        say STDERR "$level: $msg";
+    }
+}
+
 =head2 METHOD: check_for_blueprint_block_bounding
 
 =head3 DESCRIPTION:
@@ -330,15 +484,15 @@ sub new ($class) {
 =back
 
 =cut
-my sub check_for_blueprint_block_bounding ($self, @contents) {
+our sub check_for_blueprint_block_bounding ($self, @contents) {
     # trim leading and trailing whitespace...
     my $f_line = $contents[0];
     my $l_line = $contents[-1];
 
-    say STDERR "DEBUG: first: $f_line";
-    say STDERR "DEBUG: last: $l_line";
-    say STDERR "DEBUG: Keyword blueprint: $keyword{blueprint}";
-    say STDERR "DEBUG: Keyword blueprint: $keyword{end}";
+    dbgmsg("DEBUG", "first: $f_line");
+    dbgmsg("DEBUG", "last: $l_line");
+    dbgmsg("DEBUG", "Keyword blueprint: $keyword{blueprint}");
+    dbgmsg("DEBUG", "Keyword blueprint: $keyword{end}");
 
     $f_line =~ s/^\s+|\s+$//g;
     $l_line =~ s/^\s+|\s+$//g;
@@ -351,7 +505,7 @@ my sub check_for_blueprint_block_bounding ($self, @contents) {
     }
 }
 
-=head2 METHOD: parse_package_type
+=head2 METHOD: parse_pkg_type
 
 =head3 DESCRIPTION:
 
@@ -397,83 +551,205 @@ This is returned only if the package type is a known type:
 
 =back
 
+    If not, the return value is an undef value
+
+=item ARRAY REFERENCE: the updated working copy of the blueprint
+
 =back
 
 =cut
-my sub parse_package_type ($self, @bp_wc_contents) {
+our sub parse_pkg_type ($self, @bp_wc_contents) {
     foreach my $line (@bp_wc_contents) {
         $line =~ s/^\s+|\s+$//g;
         if ($line =~ m/^pkgtype:\s+(content|documentation|fonts|software)$/) {
+            # remove found item from the array
+            @bp_wc_contents = grep { $_ ne $line } @bp_wc_contents;
             $line =~ s/pkgtype:\s+//;
-            return $line;
+            return $line, \@bp_wc_contents;
         }
     }
     # otherwise...
-    say STDERR "ERROR: Uknown package type! Exiting";
-    exit 1;
+    return undef, \@bp_wc_contents;
 }
 
 =head2 METHOD: is_capability_name
 =cut
 my sub is_capability_name ($self, $string) {
+    if ($string =~ m/^/) {}
+}
+
+our sub parse_srcpkg_name ($self, $bp_wc) {
+    foreach my $line (@{$bp_wc}) {
+        $line =~ s/^\s+|\s+$//g;
+
+        # name takes a normal word value (no whitespace)
+        if ($line =~ m/^name:\s+\w+$/) {
+            dbgmsg("TRACE", "Found blueprint 'name' keyword");
+            @{$bp_wc} = grep { $_ ne $line } @{$bp_wc};
+            $line =~ s/^name:\s+//;
+            dbgmsg("TRACE", "blueprint 'name' value: $line");
+            return $line;
+        }
+    }
+    return undef;
+}
+
+our sub is_version_string ($self, $version_string) {
+    # versions can be of the following formats:
+    #  - lone digit                             example: 1
+    #  - major dot minor                        example: 1.0
+    #  - major dot minor rev-letter             example: 1.0i
+    #  - major dot minor dot patch              example: 5.3.6
+    #  - major dot minor underscore tag         example: 0.1_pre1
+    #  - series dot major dot minor dot patch   example: 6.1.100.1999
+    if ($version_string =~ m/(\d+)((\.\d+)*)([a-z]?)((_(pre|p|beta|alpha|rc)\d*)*)/) {
+        return true;
+    }
+    return false;
+}
+
+our sub parse_pkg_version ($self, $bp_wc) {
+    foreach my $line (@{$bp_wc}) {
+        $line =~ s/^\s+|\s+$//g;
+
+        if ($line =~ m/^version:\s+.*$/) {
+            dbgmsg("TRACE", "Found blueprint 'version' keyword");
+            @{$bp_wc} = grep { $_ ne $line } @{$bp_wc};
+            $line =~ s/^version:\s+//;
+            if (is_version_string($self, $line)) {
+                dbgmsg("TRACE", "blueprint 'version' value: $line");
+                return $line;
+            }
+        }
+    }
+    return undef;
+}
+
+our sub parse_pkg_release ($self, $bp_wc) {
+    foreach my $line (@{$bp_wc}) {
+        $line =~ s/^\s+|\+s$//g;
+
+        if ($line =~ m/^release:\s+\d+.*$/) {
+            @{$bp_wc} = grep { $_ ne $line } @{$bp_wc};
+            dbgmsg("TRACE", "Found blueprint 'release' keyword");
+            $line =~ s/^release:\s+//;
+            dbgmsg("TRACE", "blueprint 'release' value: $line");
+            return $line;
+        }
+    }
+    return undef;
+}
+
+our sub parse_pkg_distribution ($self, $bp_wc) {
+    foreach my $line (@{$bp_wc}) {
+        $line =~ s/^\s+|\s+$//g;
+
+        if ($line =~ m/^distribution:\s+\w+$/) {
+            @{$bp_wc} = grep { $_ ne $line } @{$bp_wc};
+            dbgmsg("TRACE", "Found blueprint 'distribution' keyword");
+            $line =~ s/^distribution:\s+//;
+            dbgmsg("TRACE", "blueprint 'distribution' value: $line");
+            return $line;
+        }
+    }
+    return undef;
+}
+
+our sub parse_pkg_license ($self, $content_type, $bp_wc) {
+    foreach my $line (@{$bp_wc}) {
+        $line =~ s/^\s+|\s+$//g;
+
+        if ($line =~ m/^license:\s+([a-zA-Z0-9\.\_\-\+\(\)]+)$/) {
+            @{$bp_wc} = grep { $_ ne $line } @{$bp_wc};
+            dbgmsg("TRACE", "Found blueprint 'license' keyword");
+            $line =~ s/license:\s+//;
+            # test that this is a valid open source license. If not,
+            #   set the taint flag
+            my $lic_validator = libLSI::LicenseValidation->new();
+            my ($valid, $class) = $lic_validator->validate($content_type, $line);
+            if ($valid == true) {
+                dbgmsg("DEBUG", "License Valitation for license $line: true");
+                return $line, $class;
+            } else {
+                dbgmsg("DEBUG", "License Valitation for license $line: false");
+                return undef, undef;
+            }
+        }
+    }
+    return undef, 0;
+}
+
+our sub parse_capabilities ($self, $string) {
+    return split(', ', $string);
+}
+
+our sub make_caps_struct ($self, @cap_list) {
+    my %cap_struct;
+    foreach my $ent (@cap_list) {
+        dbgmsg("TRACE", "cap_list entry: $ent");
+        if ($ent =~ m|^/[a-z/]+$|) {
+            dbgmsg("TRACE", "Capability type: file_path");
+            $cap_struct{"$ent"} = { 'type' => 'file_path' };
+        } elsif ($ent =~ m/^\w+$/) {
+            dbgmsg("TRACE", "Capability type: unpinned_pkg");
+            $cap_struct{"$ent"} = { 'type' => 'unpinned_pkg' };
+        } elsif ($ent =~ m|^(\w+\s+\>\<\=\!\s+.*)$|) {
+            dbgmsg("TRACE", "Capability type: pinned_pkg");
+            my ($pkg_name, $cmp_expression, $version) = split(/\s+/, $ent);
+            $cap_struct{"$pkg_name"} = { 'type' => 'pinned_pkg' };
+            $cap_struct{"$pkg_name"} = { 'cmpexp' => "$cmp_expression" };
+            $cap_struct{"$pkg_name"} = { 'version' => "$version" };
+        }
+    }
+    return %cap_struct;
+}
+
+our sub parse_pkg_build_requires ($self, $bp_wc) {
+    foreach my $line (@{$bp_wc}) {
+        $line =~ s/^\s+|\s+$//g;
+
+        if ($line =~ m/^buildrequires:\s+([\w+\,\s+]+)$/) {
+            @{$bp_wc} = grep { $_ ne $line } @{$bp_wc};
+            dbgmsg("TRACE", "Found blueprint 'buildrequires' keyword");
+            $line =~ s/buildrequires:\s+//;
+            # parse capabilities
+            my @cap_list = parse_capabilities($self, $line);
+            my %cap_struct = make_caps_struct($self, @cap_list);
+            return \%cap_struct;
+        }
+    }
+    return undef;
+}
+
+our sub parse_pkg_buildroot ($self, $bp_wc) {
 
 }
 
 my sub parse_bp_metadata ($self, $content_type, @bp_wc_contents) {
     my %bp_struct = ();
 
-    say STDERR "DEBUG: Content Type: $content_type";
+    dbgmsg("DEBUG", "Content Type: $content_type");
     # first trim leading whitespace
     foreach my $line (@bp_wc_contents) {
-        # remove unneeded leading and trailing spaces
-        $line =~ s/^\s+|\s+$//g;
-        say STDERR "DEBUG: line: $line";
-
-        # name takes a normal word value (no whitespace)
-        if ($line =~ m/^name:\s+\w+$/) {
-            say STDERR "TRACE: Found blueprint 'name' keyword";
-            $line =~ s/name:\s+//;
-            $bp_struct{'name'} = $line;
-        }
-        # version takes semver values
-        if ($line =~ m/^version:\s+\d+?(\.\d+)?(\*|\.\d+)$/) {
-            say STDERR "TRACE: Found blueprint 'version' keyword";
-            $line =~ s/version:\s+//;
-            # test that the version is an acceptable semver format
-            $bp_struct{'version'} = $line;
-        }
-        # release takes a number + a trailing distribution tag
-        if ($line =~ m/^release:\s+\d+.*$/) {
-            say STDERR "TRACE: Found blueprint 'release' keyword";
-            $line =~ s/release:\s+//;
-            # test that the release is an acceptable release value
-            $bp_struct{'release'} = $line;
-        }
-        if ($line =~ m/^distribution:\s+\w+$/) {
-            say STDERR "TRACE: Found blueprint 'distribution' keyword";
-            $line =~ s/distribution:\s+//;
-            # Is the distribution known?
-            $bp_struct{'distribution'} = $line;
-        }
-        if ($line =~ m/^license:\s+([a-zA-Z0-9\.\_\-\+\(\)]+)$/) {
-            say STDERR "TRACE: Found blueprint 'license' keyword";
-            $line =~ s/license:\s+//;
-            # test that this is a valid open source license. If not,
-            #   set the taint flag
-            my $lic_validator = libLSI::LicenseValidation->new();
-            if ($lic_validator->validate($content_type, $line) == true) {
-                say STDERR "DEBUG: License Valitation for license $line: true";
-                $bp_struct{'license'} = $line;
-            } else {
-                say STDERR "DEBUG: License Valitation for license $line: false";
-                exit 1;
-            }
-        }
-        $bp_struct{'pkgtype'} = $content_type;
-        if ($line =~ m/^buildrequires:\s+(\w+|\,|\s+)+$/) {
-            say STDERR "TRACE: Found blueprint 'buildrequires' keyword";
-        }
+        dbgmsg("DEBUG", "line: $line");
     }
+
+    $bp_struct{'pkgtype'} = $content_type;
+    $bp_struct{'srcpkg_name'}  = parse_srcpkg_name($self, \@bp_wc_contents);
+    dbgmsg("TRACE", "BP lines: ". scalar @bp_wc_contents);
+    $bp_struct{'version'}      = parse_pkg_version($self, \@bp_wc_contents);
+    dbgmsg("TRACE", "BP lines: ". scalar @bp_wc_contents);
+    $bp_struct{'release'}      = parse_pkg_release($self, \@bp_wc_contents);
+    dbgmsg("TRACE", "BP lines: ". scalar @bp_wc_contents);
+    $bp_struct{'distribution'} = parse_pkg_distribution($self, \@bp_wc_contents);
+    dbgmsg("TRACE", "BP lines: ". scalar @bp_wc_contents);
+    ($bp_struct{'license'},
+     $bp_struct{'foss'}    )   = parse_pkg_license($self, $content_type, \@bp_wc_contents);
+    dbgmsg("TRACE", "BP lines: ". scalar @bp_wc_contents);
+    $bp_struct{'buildrequires'} = parse_pkg_build_requires($self, \@bp_wc_contents);
+    dbgmsg("TRACE", "BP lines: ". scalar @bp_wc_contents);
+    $bp_struct{'buildroot'} = parse_pkg_buildroot($self, \@bp_wc_contents);
+    dbgmsg("TRACE", "BP lines: ". scalar @bp_wc_contents);
 
     return %bp_struct;
 }
@@ -481,7 +757,8 @@ my sub parse_bp_metadata ($self, $content_type, @bp_wc_contents) {
 our sub process_blueprint_file ($self, $blueprint_file) {
     my %blueprint_structure;
 
-    my $content_type = "";
+    my $content_type = undef;
+    my $bp_wc = undef;
     my $file = path($blueprint_file);
     my @blueprint_contents = $file->lines({ chomp => 1 });
     my @_blueprint_working_copy;
@@ -489,8 +766,12 @@ our sub process_blueprint_file ($self, $blueprint_file) {
     if (check_for_blueprint_block_bounding($self, @blueprint_contents) == true) {
         shift @blueprint_contents;
         pop @blueprint_contents;
-        $content_type = parse_package_type($self, @blueprint_contents);
-        %blueprint_structure = parse_bp_metadata($self, $content_type, @blueprint_contents);
+        ($content_type, $bp_wc) = parse_pkg_type($self, @blueprint_contents);
+        if (! defined $content_type) {
+            dbgmsg("ERROR", $_errors{'EWRONGPKGTYPE'}->{'msg'} . ": Exiting");
+            exit $_errors{'EWRONGPKGTYPE'}->{'code'};
+        }
+        %blueprint_structure = parse_bp_metadata($self, $content_type, @{$bp_wc});
     }
 
     return %blueprint_structure;
--- t/002_lsi_blueprint_parser.t
+++ t/002_lsi_blueprint_parser.t
@@ -4,10 +4,12 @@ use warnings;
 use libLSI::BlprntParser;
 
 use boolean;
+use Cwd;
+use Path::Tiny;
 use Test::More;
 use Test::Output;
 
-plan tests => 11;
+plan tests => 165;
 
 # Testing validate_msg_level
 my $result = undef;
@@ -31,3 +33,86 @@ stderr_is { libLSI::BlprntParser::dbgmsg("INFO", "This is a test") }
 $ENV{'LSI_DEBUG'} = undef;
 stderr_is { libLSI::BlprntParser::dbgmsg("INFO", "This is a test") }
     "", 'Testing STDERR output';
+
+# Testing process_blueprint_file
+# do we get a hash back?
+my $blpp = libLSI::BlprntParser->new();
+my $cwd = getcwd;
+my $bp_file = "$cwd/t/which.blprint";
+is(ref {$blpp->process_blueprint_file($bp_file)}, 'HASH', "Test that returned result is a hash");
+
+# Testing check_for_blueprint_block_bounding
+my $file = path($bp_file);
+my @bp = $file->lines({ chomp => 1 });
+$result = $blpp->check_for_blueprint_block_bounding(@bp);
+is($result, true, "Test that the blueprint is bounded by a Blueprint/End block");
+
+# Testing parse_pkg_type
+# positive test - throw away the array ref, since we're not going to use it
+#     in the test later
+($result, undef) = $blpp->parse_pkg_type(@bp);
+is($result, 'software', "Test parsing out the pkgtype attribute and value");
+
+# negative test
+my @bad_bp = [
+    'Blueprint',
+    'pkgtype: commercial',
+    'End'
+];
+($result, undef) = $blpp->parse_pkg_type(@bad_bp);
+is($result, undef, "Test that unknown package types returns undef");
+
+# Testing is_version_string
+# lone digit version
+$result = $blpp->is_version_string('1');
+is($result, true, "Test digit only");
+
+# major dot minor
+$result = $blpp->is_version_string('1.0');
+is($result, true, "Test major dot minor only");
+
+# major dot minor dot patch
+$result = $blpp->is_version_string('5.3.12');
+is($result, true, "Test major dot minor dot patch (semver)");
+
+# major dot minor rev-letter
+$result = $blpp->is_version_string('1.0i');
+is($result, true, "Test major dot minor rev-letter");
+
+# openssl styled versions
+$result = $blpp->is_version_string('1.1.1a');
+is($result, true, "Test major dot minor dot patch and rev-letter (openssl styled)");
+
+# series dot major dot minor dot patch (Microsoft versions and some kernels)
+$result = $blpp->is_version_string('6.2.100.1998');
+is($result, true, "Test series dot major dot minor dot patch");
+
+# version and pre-release tags
+foreach my $tag ("pre", "p", "alpha", "beta", "rc", "r",
+                 "_pre", "_p", "_alpha", "_beta", "_rc", "_r",
+                 ".pre", ".p", ".alpha", ".beta", ".rc", ".r",
+                 "-pre", "-p", "-alpha", "-beta", "-rc", "-r") {
+    # lone digit version
+    $result = $blpp->is_version_string("1${tag}1");
+    is($result, true, "Test digit only");
+
+    # major dot minor
+    $result = $blpp->is_version_string("1.0${tag}1");
+    is($result, true, "Test major dot minor only");
+
+    # major dot minor dot patch
+    $result = $blpp->is_version_string("5.3.12${tag}1");
+    is($result, true, "Test major dot minor dot patch (semver)");
+
+    # major dot minor rev-letter
+    $result = $blpp->is_version_string("1.0i${tag}1");
+    is($result, true, "Test major dot minor rev-letter");
+
+    # openssl styled versions
+    $result = $blpp->is_version_string("1.1.1a${tag}1");
+    is($result, true, "Test major dot minor dot patch and rev-letter (openssl styled)");
+
+    # series dot major dot minor dot patch (Microsoft versions and some kernels)
+    $result = $blpp->is_version_string("6.2.100.1998${tag}1");
+    is($result, true, "Test series dot major dot minor dot patch");
+}
