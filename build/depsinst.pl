bdb8d0f7668707773e5de143fb06b8924bf5ae52
Gary Greene <greeneg@tolharadys.net>
2020-02-11 21:48:38 -0500

    Use depsinst.pl to install dependencies
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

---
+++ build/depsinst.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use English;
+use utf8;
+
+my @deps = (
+    'JSON',
+    'Test::Pod',
+    'Test::Pod::Coverage',
+    'Test::Spelling',
+    'Test::Version',
+    'Pod::Coverage::TrustPod',
+    'ExtUtils::Config',
+    'ExtUtils::Helpers',
+    'ExtUtils::InstallPaths',
+    'Readonly',
+    'Perl::Critic',
+    'Test::MinimumVersion',
+    'Test::CPAN::Changes',
+    'Test::CPAN::Meta',
+    'Test::Mojibake',
+    'Test::Portability::Files',
+    'URI',
+    'boolean',
+    'List::MoreUtils',
+    'Path::Tiny',
+    'File::Slurper'
+);
+
+foreach my $dir (
+    "/__w/libLSI/.cpan",
+    "/__w/libLSI/.cpan/build",
+    "/__w/libLSI/.cpan/prefs",
+    "/__w/libLSI/.cpan/sources") {
+    if (! -d $dir) {
+        print STDOUT "Creating directory: $dir\n";
+        mkdir($dir, 0755);
+    } else {
+        print STDOUT "Directory '$dir' already exists\n";
+    }
+}
+
+foreach my $pkg (@deps) {
+    system("/usr/local/bin/cpan -j /__w/libLSI/libLSI/build/MyConfig.pm -i $pkg");
+}
+
+exit 0;
